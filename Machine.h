34e1b8ac70fe089b6fee89b2c56ff563d3157ccb
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2023-11-15 13:05:26 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -6,6 +6,8 @@
  * Section: 23
  * Date: 12 Oct 2023
  * */
+
+
 #ifndef Machine_H
 #define Machine_H
 #include "Memory.h"
@@ -15,17 +17,16 @@ class Machine
 private:
     void Get_New_Program( ); // it takes a new file from the user 
     void Display_info( ) ;// it prints the register and the memory
-    // void Set_Start_Address( ) ;
-    // void Load_all_Instructions ( ) ;//  (fetch) it takes all the instructions from the file to our vector of string
     void Load_all_Instructions_toMemory( );
-    void Load_all_Instructions_StepByStep();
+    void Load_all_Instructions_StepBySteo();
     void Reset_Machine( );
     void menu( );
 public:
     int choice = 0;
     string name_file ;
     vector<string> lines;
-    string start_address;
+    map<string ,string >lines_step;
+    string start_address = "";
     bool step = false;
     void play ();
     Machine();
