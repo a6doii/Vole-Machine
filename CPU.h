fa58870a67be08fa4df70ac4d612c247766472ec
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2023-11-15 13:04:04 +0200

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -8,41 +8,31 @@
  * */
 #ifndef CPU_H
 #define CPU_H
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class CPU {
+class CPU  {
 private :
-    int Hex_to_decimal(const string &hex);
-
-    string decimal_to_hex(int decimal);
-
+    int Hexa_to_decimal(string hexa);
+    string decimal_to_hexa(int decimal);
 public:
     CPU();
-
-    int programCounter = 0; // ( address   )   A0
+    int programcounter = 0 ; // ( addeers   )   A0    
     string IR = "0000";
     int cont = 0;
     bool stp = false;
-    char operation;
-    char register_address;
-    string x, y;
-    map<char, string> Register; // 1A34 ==> A register[ int (A) ]
-    bitset<8> addTwoNumbers(const bitset<8> &num1, const bitset<8> &num2);
-
-    bitset<8> decimalToBinary(int decimalNumber);
-
-    string binaryToHexadecimal(const bitset<8> &binaryNumber);
-
-    void Fetch(const vector<string> &lines, const string &start_address, map<string, string> memory, const string &line,
-               bool step); // give (IR) and (PC) values;
+    char operation ;
+    char registr_adress ;
+    string x,y ;
+    map<char,string > Register ; // 1A34 ==> A register[ int (A) ]
+    bitset <8> addTwoNumbers (const bitset<8>& num1, const bitset<8>& num2);
+    bitset <8>  decimalToBinary(int decimalNumber) ;
+    string binaryToHexadecimal(const bitset<8>& binaryNumber) ;
+    void Fetch(map<string ,string >lines_step,string start_adress , map<string, string> memory , string line , bool step); // give (IR) and (PC) values;
     void Valid_instruction();
-
-    void Decode();
-
-    void Execute_theInstruction(char op, map<string, string> &memory); // call instructions and memory
+    void Decod();
+    void Excute_theInstruction( char op,map<string,string> &memory ); // call instructions and memory
     void Display_CPU();
 };
 
